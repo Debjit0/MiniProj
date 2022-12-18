39762a71c300642cbc8a78065141d0a2fabfce41
Debjit Sarkar <sarkarbabudebjit@gmail.com>
2022-12-18 16:14:52 +0530

    updated databaseHelper class

--- lib/models/database_provider.dart
+++ lib/models/database_provider.dart
@@ -1,8 +1,14 @@
 import 'package:mini_proj_expense/constants/icons.dart';
+import 'package:mini_proj_expense/models/ex_category.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
+import './ex_category.dart';
 
 class DatabaseProvider {
+  //stored in ram for holding Expense categories
+  List<ExpenseCategory> _categories = [];
+  List<ExpenseCategory> get categories => _categories;
+
   Database? _database;
 
   Future<Database> get database async {
@@ -46,9 +52,35 @@ class DatabaseProvider {
       )''');
 
       //insert the initial categories
-      for (int i = 0; i < icons.length; i++) {
-        await txn.insert(cTable, {'title': icons[i]});
+      //this will add all the categories to the category table and initialise entries with 0 and total amount with 0
+      for (int i = 0; i < icons.keys.length; i++) {
+        await txn.insert(cTable, {
+          'title': icons.keys.toList()[i],
+          'entries': 0,
+          'totalAmount': (0).toString(),
+        });
       }
+    });
+  }
+
+  //method to fetch categories
+  Future<List<ExpenseCategory>> fetchCategories() async {
+    //get the database
+    final db = await database;
+    return await db.transaction((txn) async {
+      return await txn.query(cTable).then((data) {
+        //data is the fetched value
+        //convert from Map<String, object> to Map<String,dynamic>
+        final converted = List<Map<String, dynamic>>.from(data);
+        //create a ExpenseCategory from every map in this converted variable
+        List<ExpenseCategory> nList = List.generate(converted.length,
+            (index) => ExpenseCategory.fromString(converted[index]));
+        //set the values of categories to nlist
+        _categories = nList;
+
+        //return the categories
+        return _categories;
+      });
     });
   }
 }
